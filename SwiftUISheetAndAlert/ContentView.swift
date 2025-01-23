ba650ff380cac2506267324b0c6ae73b78800d9f
poga07 <chris@168money.com.tw>
2025-01-23 14:47:08 +0800

    Initial Commit

---
+++ SwiftUISheetAndAlert/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUISheetAndAlert
+//
+//  Created by 黃柏嘉 on 2025/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISheetAndAlert/SwiftUISheetAndAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISheetAndAlertApp.swift
+//  SwiftUISheetAndAlert
+//
+//  Created by 黃柏嘉 on 2025/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISheetAndAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
